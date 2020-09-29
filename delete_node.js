a8502c170e67d6f761e2c83680c345d2f1eabb6f
ella <duj_2008@163.com>
2020-09-29 14:12:44 +0800

    删除链表中的节点

--- delete_node.js
+++ delete_node.js
@@ -10,5 +10,10 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-    
+    //方法一
+    // node.val = node.next.val;
+    // node.next = node.next.next;
+
+    // 方法二
+    Object.assign(node,node.next);
 };
